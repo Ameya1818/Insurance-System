1e3c87b74d4e7bad657e12e660923ef7a924129d
Komalmagar14 <komalmagar94@gmail.com>
2025-10-22 20:57:30 +0530

    Added policy module and fixed validation

---
+++ Insurance-System/src/main/java/com/insurancesystem/service/impl/UserAddressServiceImpl.java
@@ -0,0 +1,46 @@
+/**@author Komal Gaikwad
+  *@since 2025-10-20
+  *@description Service implementation class that handles the business logic for user addresses.
+*/
+package com.insurancesystem.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.insurancesystem.entity.UserAddress;
+import com.insurancesystem.repository.UserAddressRepository;
+
+@Service
+public class UserAddressServiceImpl {
+
+	@Autowired
+	private final UserAddressRepository repository;
+
+    public UserAddressServiceImpl(UserAddressRepository repository) {
+        this.repository = repository;
+    }
+
+    public UserAddress saveAddress(UserAddress address) {
+        return repository.save(address);
+    }
+
+    public List<UserAddress> getAllAddresses() {
+        return repository.findAll();
+    }
+
+    public Optional<UserAddress> getAddressById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<UserAddress> getAddressesByUserId(Long userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public void deleteAddress(Long id) {
+        repository.deleteById(id);
+    }
+
+}
